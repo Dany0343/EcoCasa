ee11fe290fe97223cca584ecf53e0fb730e42e36
Oscar Bucio <oscarbucio2001@gmail.com>
2023-11-21 19:30:49 -0600

    login partially working

--- src/app/closeSession.tsx
+++ src/app/closeSession.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {signOut} from 'next-auth/react'
+import { signOut } from 'next-auth/react'
 
 export default function CloseSession() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
+import CloseSession from "./closeSession";
 
 export default function Home() {
   return (
-    <h1>Hola mundo!</h1>
-  )
+    <>
+      <h1>Hola mundo!</h1>
+      <CloseSession />
+    </>
+  );
 }
