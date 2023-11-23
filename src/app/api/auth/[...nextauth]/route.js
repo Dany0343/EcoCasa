7ebad65af7957be1921361b67142806cfd8fcb82
Oscar Bucio <oscarbucio2001@gmail.com>
2023-11-23 11:55:03 -0600

    right export to ship

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,49 @@
-import { authOptions } from '@/utils/authOptions'
-import { NextAuth } from 'next-auth/next'
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import db from "@/libs/db";
+import bcrypt from "bcrypt";
+
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password", placeholder: "*****" },
+      },
+      async authorize(credentials, req) {
+        console.log(credentials);
+
+        const userFound = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!userFound) throw new Error("No user found");
+
+        console.log(userFound);
+
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.password
+        );
+
+        if (!matchPassword) throw new Error("Wrong password");
+
+        return {
+          id: userFound.id,
+          name: userFound.username,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
 const handler = NextAuth(authOptions);
 
