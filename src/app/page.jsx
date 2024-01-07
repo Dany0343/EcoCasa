3a44dd95cab779af860d1352f638f4e897932832
Oscar Bucio <oscarbucio2001@gmail.com>
2024-01-07 04:18:55 -0600

    holy fuck its working

---
+++ src/app/page.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+// Importaciones necesarias
+import React, { useEffect, useState } from "react";
+import Filters from "../components/Filters";
+import Card from "../components/Card";
+import Link from "next/link";
+
+export default function Home() {
+  const [houses, setHouses] = useState([]);
+  const [filters, setFilters] = useState({
+    location: "",
+    priceMin: "",
+    priceMax: "",
+    area: "",
+    bedrooms: "",
+    bathrooms: "",
+  });
+
+  useEffect(() => {
+    async function fetchHouses() {
+      const response = await fetch("api/houses");
+      const data = await response.json();
+      setHouses(data);
+    }
+
+    fetchHouses();
+  }, []);
+
+  const handleFilterChange = (filterName, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filterName]: value,
+    }));
+  };
+
+  const filteredHouses = houses.filter((house) => {
+    return (
+      (filters.location
+        ? house.location.toLowerCase().includes(filters.location.toLowerCase())
+        : true) &&
+      (filters.priceMin ? house.price >= parseFloat(filters.priceMin) : true) &&
+      (filters.priceMax ? house.price <= parseFloat(filters.priceMax) : true) &&
+      (filters.area ? house.area >= parseFloat(filters.area) : true) &&
+      (filters.bedrooms
+        ? house.bedrooms === parseInt(filters.bedrooms)
+        : true) &&
+      (filters.bathrooms
+        ? house.bathrooms === parseInt(filters.bathrooms)
+        : true)
+    );
+  });
+
+  return (
+    <>
+      <div className="container mx-auto px-4 mt-8">
+        <Filters filters={filters} onFilterChange={handleFilterChange} />
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
+          {filteredHouses.map((house, index) => (
+            <Link
+              href={{
+                pathname: "/house",
+                query: { ...house },
+              }}
+              key={index}
+            >
+              <Card {...house} />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,41 +1,49 @@
 import React from 'react';
 
-function Filters() {
+function Filters({ filters, onFilterChange }) {
   return (
     <div className="bg-white p-4 rounded-md shadow-md">
-      {/* Filtros principales en una fila */}
       <div className="flex flex-col md:flex-row md:items-end gap-4 mb-4">
-        {/* Búsqueda por ubicación */}
         <input
+          value={filters.location}
+          onChange={(e) => onFilterChange('location', e.target.value)}
           className="form-input border p-2 rounded w-full"
           type="text"
           placeholder="Buscar por colonia, zona o ciudad"
         />
 
-        {/* Rango de precios */}
         <div className="flex flex-col md:flex-row w-full gap-4">
           <input
+            value={filters.priceMin}
+            onChange={(e) => onFilterChange('priceMin', e.target.value)}
             className="form-input border p-2 rounded w-full md:w-1/2"
             type="number"
             placeholder="Precio Mínimo"
           />
           <input
+            value={filters.priceMax}
+            onChange={(e) => onFilterChange('priceMax', e.target.value)}
             className="form-input border p-2 rounded w-full md:w-1/2"
             type="number"
             placeholder="Precio Máximo"
           />
         </div>
+
         <input
+            value={filters.area}
+            onChange={(e) => onFilterChange('area', e.target.value)}
             className="form-input border p-2 rounded w-full md:w-1/2"
             type="number"
             placeholder="Area"
-          />
+        />
       </div>
 
-      {/* Filtros secundarios en una segunda fila */}
       <div className="flex flex-col md:flex-row w-full gap-4 mb-4">
-        {/* Selección de habitaciones */}
-        <select className="form-select border p-2 rounded w-full">
+        <select 
+          value={filters.bedrooms}
+          onChange={(e) => onFilterChange('bedrooms', e.target.value)}
+          className="form-select border p-2 rounded w-full"
+        >
           <option value="">Habitaciones</option>
           <option value="1">1 Habitación</option>
           <option value="2">2 Habitaciones</option>
@@ -43,23 +51,20 @@ function Filters() {
           <option value="4">4+ Habitaciones</option>
         </select>
 
-        {/* Selección de baños */}
-        <select className="form-select border p-2 rounded w-full">
+        <select 
+          value={filters.bathrooms}
+          onChange={(e) => onFilterChange('bathrooms', e.target.value)}
+          className="form-select border p-2 rounded w-full"
+        >
           <option value="">Baños</option>
           <option value="1">1 Baño</option>
           <option value="2">2 Baños</option>
           <option value="3">3+ Baños</option>
         </select>
       </div>
-
-      {/* Botón de búsqueda */}
-      {/* <div className="flex justify-center">
-        <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition">
-          Buscar
-        </button>
-      </div> */}
     </div>
   );
 }
 
 export default Filters;
+
